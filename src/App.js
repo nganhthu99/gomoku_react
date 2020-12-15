352fa5a3e0d32e3de883f9871ff817cf6a5c883f
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-12-15 18:47:36 +0700

    Update routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import SignIn from "./sign-in";
+import SignIn from "./Authentication/sign-in";
 import React from "react";
-import SignUp from "./sign-up";
-import Home from "./home";
+import SignUp from "./Authentication/sign-up";
+import Home from "./Home/home";
 import { Switch, Route } from 'react-router-dom';
+import Game from "./Game/game";
 
 function App() {
   return (
       <Switch>
-        {/*<Route path='/home' component={Home}/>*/}
+        <Route path='/game' component={Game}/>
         <Route path='/sign-up' component={SignUp}/>
         <Route path='/sign-in' component={SignIn}/>
         <Route path='/' component={Home}/>
--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
       <BrowserRouter>
             <App />
       </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+,  document.getElementById('root')
 );
 
