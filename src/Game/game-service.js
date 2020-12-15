37c2d3116c6a74bd18709f696ea6fb85377c7917
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-12-15 18:47:02 +0700

    Add game logic check win, check full

---
+++ src/Game/game-service.js
@@ -0,0 +1,144 @@
+export const calculateWinner = function (squares, step, boardSize, playerMove) {
+    if (step[0] === null && step[1] === null)
+        return {
+            status: undefined,
+            line: []
+        }
+
+    let matrix = new Array(boardSize).fill(null)
+    for (let i = 0; i < matrix.length; i++) {
+        matrix[i] = new Array(boardSize).fill(null)
+    }
+
+    for (let i = 0; i < boardSize; i++) {
+        for (let j = 0; j < boardSize; j++) {
+            matrix[i][j] = squares[i * boardSize + j]
+        }
+    }
+
+    let row = step[0]
+    let column = step[1]
+
+    let winState = (boardSize < 5) ? 3 : 5
+
+    // check row
+    let line=[step[0] * boardSize + step[1]]
+    let countLeft = 0;
+    let countRight = 0;
+    for (let i = column - 1; i >= 0; i--) {
+        if (matrix[row][i] === playerMove) {
+            countLeft++;
+            line.push(row*boardSize+i)
+        } else break;
+    }
+    for (let i = column + 1; i < boardSize; i++) {
+        if (matrix[row][i] === playerMove) {
+            countRight++;
+            line.push(row*boardSize+i)
+        } else break;
+    }
+    if (countLeft + countRight === winState - 1)
+        return {
+            status: playerMove,
+            line: line,
+        };
+
+    //check column
+    line=[step[0] * boardSize + step[1]]
+    countLeft = 0;
+    countRight = 0;
+    for (let i = row - 1; i >= 0; i--) {
+        if (matrix[i][column] === playerMove) {
+            countLeft++;
+            line.push(i*boardSize+column)
+        } else break;
+    }
+    for (let i = row + 1; i < boardSize; i++) {
+        if (matrix[i][column] === playerMove) {
+            countRight++;
+            line.push(i*boardSize+column)
+        } else break;
+    }
+    if (countLeft + countRight === winState - 1)
+        return {
+            status: playerMove,
+            line: line,
+        };
+
+    //check diag
+    line=[step[0] * boardSize + step[1]]
+    countLeft = 0;
+    countRight = 0;
+    let i = row - 1;
+    let j = column - 1;
+    while (i >= 0 && j >= 0) {
+        if (matrix[i][j] === playerMove) {
+            countLeft++;
+            line.push(i*boardSize+j)
+        } else break;
+        i--;
+        j--;
+    }
+    i = row + 1;
+    j = column + 1;
+    while (i < boardSize && j < boardSize) {
+        if (matrix[i][j] === playerMove) {
+            countRight++;
+            line.push(i*boardSize+j)
+        } else break;
+        i++;
+        j++;
+    }
+    if (countLeft + countRight === winState - 1)
+        return {
+            status: playerMove,
+            line: line,
+        };
+
+    //check anti diag
+    line=[step[0] * boardSize + step[1]]
+    countLeft = 0;
+    countRight = 0;
+    i = row + 1;
+    j = column - 1;
+    while (i < boardSize && j >= 0) {
+        if (matrix[i][j] === playerMove) {
+            countLeft++;
+            line.push(i*boardSize+j)
+        } else break;
+        i++;
+        j--;
+    }
+    i = row - 1;
+    j = column + 1;
+    while (i >= 0 && j < boardSize) {
+        if (matrix[i][j] === playerMove) {
+            countRight++;
+            line.push(i*boardSize+j)
+        } else break;
+        i--;
+        j++;
+    }
+    if (countLeft + countRight === winState - 1)
+        return {
+            status: playerMove,
+            line: line,
+        };
+    if (checkBoardFull(squares))
+        return {
+            status: 'draw',
+            line: []
+        }
+    else return {
+            status: undefined,
+            line: []
+        }
+}
+
+export const checkBoardFull = function(squares) {
+    for (let i = 0; i < squares.length; i++) {
+        if (squares[i] === null)
+            return false
+    }
+    return true
+}
