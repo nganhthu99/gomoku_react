2255c7c3b0f860fae3747b91de9fc126e1b3579d
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-12-16 14:47:53 +0700

    Add socket context to app.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import SignUp from "./Authentication/sign-up";
 import Home from "./Home/home";
 import { Switch, Route } from 'react-router-dom';
 import Game from "./Game/game";
+import {SocketProvider} from "./socket-provider";
 
 function App() {
   return (
-      <Switch>
-        <Route path='/game' component={Game}/>
-        <Route path='/sign-up' component={SignUp}/>
-        <Route path='/sign-in' component={SignIn}/>
-        <Route path='/' component={Home}/>
-      </Switch>
+      <SocketProvider>
+          <Switch>
+                <Route path='/game' component={Game}/>
+                <Route path='/home' component={Home}/>
+                <Route path='/sign-up' component={SignUp}/>
+                <Route path='/sign-in' component={SignIn}/>
+                <Route path='/' component={SignIn}/>
+          </Switch>
+      </SocketProvider>
   );
 }
 
